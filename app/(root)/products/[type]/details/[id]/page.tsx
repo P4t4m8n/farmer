a61f41ee8c8786522b6e2c0aa674d34d2d3f5c21
P4t4m8n <michaelieran@gmail.com>
2024-11-23 20:47:08 +0200

    addn deail route

--- app/(root)/products/[type]/details/[id]/page.tsx
+++ app/(root)/products/[type]/details/[id]/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import ProductDetailsModel from "@/components/Product/ProductDetails/ProductDetails";
+import { getProductById } from "@/lib/actions/product.actions";
+import { redirect } from "next/navigation";
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+export default async function page({
+  params,
+}: {
+  params: Promise<{ id: string; type: string }>;
+}) {
+  const { id, type } = await params;
+
+  const product = await getProductById(id, false);
+  if (!product) return redirect(`/products/${type}`);
+
+  return <ProductDetailsModel product={product} />;
 }
